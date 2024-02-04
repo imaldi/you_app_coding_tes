1adcf6bfff33309f2e5020b79109dbddf652678a
aldi <aldiirsanmajid@gmail.com>
2024-02-04 17:02:14 +0700

    completing profile remote data source and repository impl

--- lib/features/profile/data/repository/profile_repository_impl.dart
+++ lib/features/profile/data/repository/profile_repository_impl.dart
@@ -1,32 +1,69 @@
+import 'package:dio/dio.dart';
 import 'package:fpdart/src/either.dart';
-
 import 'package:you_app_coding_tes/core/failures/app_failures.dart';
-
-import 'package:you_app_coding_tes/features/profile/domain/entity/user_entity.dart';
+import 'package:you_app_coding_tes/core/network/api_error.dart';
+import 'package:you_app_coding_tes/features/profile/data/source/model/user_model.dart';
 
 import '../../domain/repository/profile_repository.dart';
 import '../profile_remote_data_source.dart';
 
 class ProfileRepositoryImpl extends ProfileRepository {
-
   final ProfileRemoteDataSource _remoteDataSource;
+
   ProfileRepositoryImpl(this._remoteDataSource);
+
   @override
-  Future<Either<AppFailure, bool>> createProfile(User user) {
-    // TODO: implement createProfile
-    throw UnimplementedError();
+  Future<Either<AppFailure, bool>> createProfile(String? name, String? birthday,
+      String? height, String? weight, List<String>? interests) async {
+    try {
+      await _remoteDataSource.createProfile(UserApiModel(
+        name: name,
+        birthday: birthday,
+        height: height,
+        weight: weight,
+        interests: interests,
+      ));
+      return Right(true);
+    } on DioException catch (e) {
+      return Left(ApiError(
+          statusCode: e.response?.statusCode,
+          message: e.response?.statusMessage));
+    }
   }
 
   @override
-  Future<Either<AppFailure, bool>> getProfile(User user) {
-    // TODO: implement getProfile
-    throw UnimplementedError();
+  Future<Either<AppFailure, bool>> getProfile() async {
+    try {
+      await _remoteDataSource.getProfile();
+      return const Right(true);
+    } on DioException catch (e) {
+      return Left(ApiError(
+          statusCode: e.response?.statusCode,
+          message: e.response?.statusMessage));
+    }
   }
 
   @override
-  Future<Either<AppFailure, bool>> updateProfile(User user) {
-    // TODO: implement updateProfile
-    throw UnimplementedError();
+  Future<Either<AppFailure, bool>> updateProfile(
+    String? name,
+    String? birthday,
+    String? height,
+    String? weight,
+    List<String>? interests,
+  ) async {
+    try {
+      await _remoteDataSource.updateProfile(UserApiModel(
+        name: name,
+        birthday: birthday,
+        height: height,
+        weight: weight,
+        interests: interests,
+      ));
+      return const Right(true);
+    } on DioException catch (e) {
+      return Left(ApiError(
+          statusCode: e.response?.statusCode,
+          message: e.response?.statusMessage));
+    }
   }
-
 }
--- lib/features/profile/domain/repository/profile_repository.dart
+++ lib/features/profile/domain/repository/profile_repository.dart
@@ -1,11 +1,24 @@
 import 'package:fpdart/fpdart.dart';
 
 import '../../../../core/failures/app_failures.dart';
-import '../entity/user_entity.dart';
 
 abstract class ProfileRepository {
   // FIX me is it okay to use entity here?
-  Future<Either<AppFailure,bool>> createProfile(User user);
-  Future<Either<AppFailure,bool>> getProfile(User user);
-  Future<Either<AppFailure,bool>> updateProfile(User user);
+  Future<Either<AppFailure, bool>> createProfile(
+    String? name,
+    String? birthday,
+    String? height,
+    String? weight,
+    List<String>? interests,
+  );
+
+  Future<Either<AppFailure, bool>> getProfile();
+
+  Future<Either<AppFailure, bool>> updateProfile(
+    String? name,
+    String? birthday,
+    String? height,
+    String? weight,
+    List<String>? interests,
+  );
 }
